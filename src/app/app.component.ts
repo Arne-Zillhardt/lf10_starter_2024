0383375ad391d3151a9b912ca24096f04ffb296e
lucafynntasto <luca.tasto@neuland-bfi.de>
2025-01-15 12:38:23 +0100

    sidebar/navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {EmployeeListComponent} from "./employee-list/employee-list.component";
 import {AddEmployeeComponent} from "./employee-form/add-employee.component";
+import {SidebarComponent} from "./elements/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, EmployeeListComponent, AddEmployeeComponent, RouterOutlet, RouterLink],
+  imports: [CommonModule, EmployeeListComponent, AddEmployeeComponent, RouterOutlet, RouterLink, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/skill-list/skill-list.component.ts
+++ src/app/skill-list/skill-list.component.ts
@@ -15,7 +15,7 @@ import {NgForOf, NgIf} from "@angular/common";
 })
 export class SkillListComponent {
   data = [
-    { id: 1, name: 'Bitte neuen Namen eingeben...', mitgliederanzahl: 10, isEditing: false },
+    { id: 1, name: 'Docker', mitgliederanzahl: 10, isEditing: false },
     { id: 2, name: 'React', mitgliederanzahl: 6, isEditing: false },
     { id: 3, name: 'Mongo', mitgliederanzahl: 0, isEditing: false },
     { id: 4, name: 'Java', mitgliederanzahl: 24, isEditing: false },
