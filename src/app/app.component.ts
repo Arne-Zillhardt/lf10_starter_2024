52ee1ffde3c9e79b5db22ae0469bdbced00c0cb0
lucafynntasto <luca.tasto@neuland-bfi.de>
2025-01-15 10:41:04 +0100

    html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {EmployeeListComponent} from "./employee-list/employee-list.component";
+import {AddEmployeeComponent} from "./add-employee/add-employee.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, EmployeeListComponent],
+  imports: [CommonModule, EmployeeListComponent, AddEmployeeComponent, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'add-employee', component: AddEmployeeComponent },
+  { path: 'employee/:id', component: AddEmployeeComponent },
+  { path: '', component: EmployeeListComponent }
+];
