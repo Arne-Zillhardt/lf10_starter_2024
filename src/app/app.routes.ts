d9c42ec95797da87c0c1724f8d34d274b15a4768
lucafynntasto <luca.tasto@neuland-bfi.de>
2025-01-15 13:11:30 +0100

    sidebar/navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
 import {SkillListComponent} from "./skill-list/skill-list.component";
 
 export const routes: Routes = [
-  { path: 'add-employee', component: AddEmployeeComponent },
-  { path: 'edit-employee/:id', component: AddEmployeeComponent },
+  { path: 'employees/add', component: AddEmployeeComponent },
+  { path: 'employees/edit/:id', component: AddEmployeeComponent },
   { path: 'employees', component: EmployeeListComponent },
   { path: 'skills', component: SkillListComponent },
   { path: '', component: EmployeeListComponent }
