3334943deac201e9beadfdb8ac64578793cc9478
Nikita Hertrampf <nikita.h4@schule.bremen.de>
2025-01-16 08:34:25 +0100

    feature/Add-Skill add add-skill logic and edit-skill

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {EmployeeListComponent} from "./employee-list/employee-list.component";
+import {SkillsComponent} from "./skills/skills.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/skills', pathMatch: 'full' }, // Standardroute
+  { path: 'skills', component: SkillsComponent },         // Route für Skills
+  { path: 'employees', component: EmployeeListComponent } // Route für Employees
+];
