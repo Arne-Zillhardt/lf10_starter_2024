c3903ddea7b63bd4cbc81283096ba2ab4b19a5fa
lucafynntasto <luca.tasto@neuland-bfi.de>
2025-01-16 11:03:09 +0100

    CSS Employee form, Skill selector

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {Observable, of} from "rxjs";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Employee} from "../Employee";
 import {RouterLink} from "@angular/router";
 
 @Component({
@@ -13,19 +10,4 @@ import {RouterLink} from "@angular/router";
   styleUrl: './employees.component.css'
 })
 export class EmployeesComponent {
-  bearer = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIzUFQ0dldiNno5MnlQWk1EWnBqT1U0RjFVN0lwNi1ELUlqQWVGczJPbGU0In0.eyJleHAiOjE3MzY5MzAyMTcsImlhdCI6MTczNjkyNjYxNywianRpIjoiZjU4N2JiOWEtNDAwMy00NDY0LTg3NjItZDRkNjhmZWJiYTNhIiwiaXNzIjoiaHR0cHM6Ly9rZXljbG9hay5zenV0LmRldi9hdXRoL3JlYWxtcy9zenV0IiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjU1NDZjZDIxLTk4NTQtNDMyZi1hNDY3LTRkZTNlZWRmNTg4OSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImVtcGxveWVlLW1hbmFnZW1lbnQtc2VydmljZSIsInNlc3Npb25fc3RhdGUiOiIxNDgyZjRjMS00YjhmLTQ4MDYtYTQ1Zi1hM2Y4OGYzODNhOTEiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHA6Ly9sb2NhbGhvc3Q6NDIwMCJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsicHJvZHVjdF9vd25lciIsIm9mZmxpbmVfYWNjZXNzIiwiZGVmYXVsdC1yb2xlcy1zenV0IiwidW1hX2F1dGhvcml6YXRpb24iLCJ1c2VyIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInByZWZlcnJlZF91c2VybmFtZSI6InVzZXIifQ.BVGmBg6BXBuljZAnCIamwtrixhYG8zvtlkZ_IVxRFl3cPxTGTlc0qUPX1KyEMED5D3FtEPvxzJMeCFfvKxTSFPAV7x4k9Zf9uUODUPOIKZBQ_Vunck29lgug5rFjwkiLay62ap1UkhC3vJ-6tYBv-vlSNOTDCzeKfLaxoUtj9qToqYHOujDZ_lVwgHCjp5WHWNoJisW1XmjSuOCGOJ0Joaj3HT7Afy3Gd0WFyQ8-wQUi6jxpEW2-0h4GdAzZDdMBkq86KJuc0p8-3p91WThDywK9YSOX4IyqSrpjsavKBFBwiyGV5UfZNHOEWQZyudy3zVRKnDqSP5qDdkTiuN5oQA';
-  employees$: Observable<Employee[]>;
-
-  constructor(private http: HttpClient) {
-    this.employees$ = of([]);
-    this.fetchData();
-  }
-
-  fetchData() {
-    this.employees$ = this.http.get<Employee[]>('http://localhost:8089/employees', {
-      headers: new HttpHeaders()
-        .set('Content-Type', 'application/json')
-        .set('Authorization', `Bearer ${this.bearer}`)
-    });
-  }
 }
