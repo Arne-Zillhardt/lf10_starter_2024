6a664031437659bf8080a958f90863f74d676ccb
Arne Paul Zillhardt <arne@zillhardt.com>
2025-01-22 12:33:10 +0100

    Dashboar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { EmployeeFormComponent } from './employee-form/employee-form.component';
 import { EmployeesComponent } from './employees/employees.component';
 import {SkillsComponent} from "./skills/skills.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 export const routes: Routes = [
   { path: 'employees/add', component: EmployeeFormComponent },
   { path: 'employees/edit/:id', component: EmployeeFormComponent },
   { path: 'employees', component: EmployeesComponent },
   { path: 'skills', component: SkillsComponent },
-  { path: '', component: EmployeesComponent }
+  { path: '', component: DashboardComponent}
 ];
